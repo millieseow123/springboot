64dc46c8a94b9a8724553142e6209b34615c70e7
Millie <millieseow@outlook.com>
2022-11-16 03:05:25 +0800

    commit

--- angular/proxy-config.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-    {
-        context: ['/**'],
-        target: 'http://localhost:8080',
-        secure: false
-    }
-]
--- angular/src/app/models.ts
+++
@@ -1,8 +0,0 @@
-export interface RegisterUser {
-    email: string
-    password: string
-    confirmPassword: string
-    firstName: string
-    lastName: string
-    profilePicture: File
-}
